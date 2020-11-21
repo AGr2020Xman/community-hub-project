721d6b72db4eec6e7e3382d7d9c4d0afdd8e6a2f
Andre Grech <silver.grech@gmail.com>
2020-11-22 02:01:01 +1100

    users model

--- models/userdata.js
+++ models/userdata.js
@@ -5,7 +5,60 @@ const sequelize = new Sequelize('database', 'username', 'password', {
     dialect: 'mysql'
 });
 
-class User extends Model {}
+class User extends Model {
+    static getFullName() {
+        return [this.firstname, this.lastName].join(' ');
+    }
+}
+
+User.init({
+    firstName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            is: /^[a-z]+$/i,
+            max: 25
+        }
+    },
+    lastName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            is: /^[a-z]+$/i,
+            max: 25
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true, 
+        validate: {
+            isEmail: true
+        }
+    },
+    username: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true,
+            is: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        }
+    },
+    uniqueIdentifier: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4
+    }
+
+},
+{
+    sequelize,
+    modelName: 'User'
+});
 
 
 

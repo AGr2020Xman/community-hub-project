4da52f151a2c6b5752752333bf5575e4837fd9af
Andre Grech <silver.grech@gmail.com>
2020-11-22 02:20:31 +1100

    adding passport authenticated routes in controller for logging in

---
+++ controllers/loginpage/login.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const app = express();
+
+initialisePassport(
+  passport, 
+  email => db.find(user => user.email === email),
+  id => db.find(user => user.id === id)
+);
+
+// middlware
+const checkAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  res.redirect('/login');
+}
+
+const checkNotAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+      return res.redirect('/');
+  }
+  next();
+}
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('index.handlebars', {name: req.user.name})
+});
+
+app.get('/login',checkNotAuthenticated, (req, res) => {
+  res.render('login.handlebars')
+})
+
+app.get('/register',checkNotAuthenticated, (req, res) => {
+  res.render('signup.handlebars')
+})
+
+app.post('/login',checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true,
+}))
+
+app.post('/register',checkNotAuthenticated, async (req, res) => {
+  try{
+      const protectedPsw = await bcrpyt.hash(req.body.password, 10);
+      tempDB.push({
+          id: Date.now().toString(),
+          name: req.body.firstName + " " + req.body.lastName,
+          email: req.body.email,
+          username: req.body.username,
+          password: protectedPsw,
+      })
+      res.redirect('/login')
+  } catch (err) {
+    if (err) console.log('There was an error registering user:\n', err);
+    res.redirect('/signup')
+  }
+});
+
+// logout function
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+});
+
+
+
+// protected (auth needed) - unprotected routes
+// route guards - ? certain routes only working when lgged in
+// component guard - ? (the create button requires authenticated user)

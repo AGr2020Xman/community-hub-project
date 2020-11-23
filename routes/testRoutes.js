4410d43ab139e308ca2328220542394c89936299
Andre Grech <silver.grech@gmail.com>
2020-11-23 21:26:24 +1100

    remove test routes

--- routes/testRoutes.js
+++
@@ -1,9 +0,0 @@
-// Dependencies
-const path = require("path");
-
-// Routing
-module.exports = (app) => {
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/placeholder.html"));
-  });
-};

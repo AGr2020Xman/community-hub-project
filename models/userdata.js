914f2e66c9d9c0debf3d623bcf8e3d6b4e9f09a8
Andre Grech <silver.grech@gmail.com>
2020-11-25 03:16:52 +1100

    not extending model. change from .init to .define method for now. was missing return USER thus the original code break.

--- models/userdata.js
+++ models/userdata.js
@@ -1,9 +1,9 @@
-const { Sequelize, DataTypes, Model } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 const sequelize = new Sequelize('geo_verse_db', 'root', 'H0n@s0up1234', {
     host: '127.0.0.1',
     dialect: 'mysql',
-    operatorsAliases: false
 });
 
 sequelize.authenticate().then((err)=>{
@@ -12,73 +12,62 @@ sequelize.authenticate().then((err)=>{
     console.log('Unable to connect to db', err);
 })
 
-class User extends Model {
-    // static getFullName() {
-    //     return [this.firstname, this.lastName].join(' ');
-    // }
-};
-
-User.init({
-    firstName: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-            is: /^[a-z]+$/i,
-            max: 25
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        firstName: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                is: /^[a-z]+$/i,
+                max: 25
+            }
+        },
+        lastName: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                is: /^[a-z]+$/i,
+                max: 25
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true, 
+            validate: {
+                isEmail: true
+            }
+        },
+        nickname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            max: 20
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                is: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                max: 128
+            }
+        },
+        uniqueIdentifier: {
+            type: DataTypes.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            unique: true
         }
     },
-    lastName: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-        validate: {
-            is: /^[a-z]+$/i,
-            max: 25
-        }
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true, 
-        validate: {
-            isEmail: true
-        }
-    },
-    nickname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        max: 20
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            notEmpty: true,
-            is: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-            max: 128
-        }
-    },
-    uniqueIdentifier: {
-        type: DataTypes.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        unique: true
-    }
-},
-{
-    hooks: {
-        beforeCreate: (user, options) => {
-            user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
-        }
-    }
-},
-{
-    sequelize: 'sequelize',
-    modelName: 'User'
-});
-
-console.log(User === sequelize.models.User);
-
-module.exports = sequelize;
-
-
-
+    {
+        sequelize,
+        hooks: {
+            beforeCreate: async (user, options) => {
+                user.password = await bcrypt.hash(user.password, 10, null);
+                console.log('should be hashed', user.password);
+            }
+        },
+        modelName: 'User',
+    });
+    return User
+  };

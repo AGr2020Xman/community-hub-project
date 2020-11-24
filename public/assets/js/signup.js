8fe95364c071324418d227d261d672e664e500f0
Andre Grech <silver.grech@gmail.com>
2020-11-25 03:17:39 +1100

    client handling of signup form

---
+++ public/assets/js/signup.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  // Getting references to our form and input
+  const signUpForm = $("form.signup");
+  const emailInput = $("input#email");
+  const passwordInput = $("input#password");
+  const nicknameInput = $("input#nickname");
+  const firstNameInput = $("input#firstName");
+  const lastNameInput = $("input#lastName");
+
+console.log('TESTING');
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    console.log(emailInput, passwordInput, nicknameInput);
+    const userData = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
+      email: emailInput.val().trim(),
+      nickname: nicknameInput.val().trim(),
+      password: passwordInput.val().trim(),
+    };
+
+    if (!userData.firstName || !userData.lastName || !userData.email || !userData.nickname || !userData.password) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.firstName, userData.lastName, userData.email, userData.nickname, userData.password);
+    emailInput.val("");
+    passwordInput.val("");
+    nicknameInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  const signUpUser = (firstName, lastName, email, nickname, password) => {
+    console.log('OVER HERE', firstName, lastName, email, nickname, password);
+    $.post("/api/signup", {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      nickname: nickname,
+      password: password
+    })
+      .then(function(data) {
+        window.location.replace("/login");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  const handleLoginErr = (err) => {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});

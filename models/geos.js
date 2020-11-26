f02355aaad104f3def7a60899a3a8ae4ce838a90
Andre Grech <silver.grech@gmail.com>
2020-11-26 16:07:38 +1100

    geos model with many communities - onDelete? to be decided

---
+++ models/geos.js
@@ -0,0 +1,37 @@
+const { Sequelize, Model } = require('sequelize');
+const bcrypt = require('bcrypt');
+
+// const sequelize = new Sequelize('geo_verse_db', 'root', 'H0n@s0up1234', {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+// });
+
+// sequelize.authenticate().then((err)=>{
+//     console.log('Connection Successful');
+// }).catch((err)=>{
+//     console.log('Unable to connect to db', err);
+// })
+
+module.exports = (sequelize, DataTypes) => {
+
+    class Geo extends Model {
+        static associate(models) {
+            Geo.hasMany(models.Community, { as: 'communities', onDelete: ''})
+        }
+    }
+    Geo.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                max: 50,
+                unique: true,
+            }
+        },
+    {
+        hooks: {},
+        sequelize,
+        modelName: 'Geo',
+    }
+    );
+    return Geo
+  };

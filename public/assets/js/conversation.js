147dae0c64056c7db8267c2436f715dad3beb0e1
Harish Narain <harishnarain@gmail.com>
2020-11-26 22:33:20 +1100

    Updated new conversations to have a conversationId of a random GUID temporarily.

--- public/assets/js/conversation.js
+++ public/assets/js/conversation.js
@@ -7,7 +7,14 @@ let conversationId;
 if (id) {
   conversationId = id;
 } else {
-  conversationId = "randomguid";
+  conversationId = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+    /[xy]/g,
+    (c) => {
+      const r = (Math.random() * 16) | 0,
+        v = c == "x" ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    }
+  );
 }
 
 // Elements

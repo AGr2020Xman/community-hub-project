3b3da4aa7ec514c74ac0ab64979ffe64d28a12ce
Andre Grech <silver.grech@gmail.com>
2020-11-30 03:52:59 +1100

    adjusting clashing routes - adding update fx

--- controllers/usersapi/api-user-routes.js
+++ controllers/usersapi/api-user-routes.js
@@ -1,70 +1,48 @@
 const { checkAuthenticated } = require('../../config/middleware/checkAuth');
 const { typeOfQueryData } = require('./api-user-query');
 const express = require('express');
+const db = require('../../models');
 const router = express.Router();
 
+// find a user by full name, nick name, or 'partial' name, or UUID
+router.post('/api/users', checkAuthenticated, async (req, res) => {
+  if (!req.query || req.query === 0) {
+    res.json({});
+  } else {
+    typeOfQueryData(req, res);
+  }
+});
 
-router.get('/api/users', checkAuthenticated, async (req, res) => {
-    console.log(req.query);
-    if (req.query == " " || req.query === "") {
-        let response = 'Please enter a term to search'
-        res.json(response);
-    } else {
-        typeOfQueryData(req, res)
-    }
+router.put('/api/users', checkAuthenticated, async (req, res) => {
+  console.log('USER BEFORE UPDATE', req.user.uniqueIdentifier);
+  if (!req.user) {
+    res.sendStatus(401);
+    return;
+  }
+  try {
+    db.User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+      },
+      {
+        where: {
+          uniqueIdentifier: req.user.uniqueIdentifier,
+        },
+      }
+    ).then((dbUser) => {
+      res.status(201).json(dbUser);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/api/users', checkAuthenticated, async (req, res) => {
+  db.User.destroy({
+    where: {},
+  });
 });
 
 module.exports = router;
-   
-
-//
-
-// const usersObj = {
-//     firstName: searchedUser.firstName,
-//     lastname: searchedUser.lastname,
-//     nickname: searchedUser.nickname,
-//     uniqueidentifier: searchedUser.uniqueidentifier
-// }
-// array OR obj of users, for searching ALL users
-// wants names, nickname, uuid,
-// i want to search by either firstname or lastname or nickname
-// return back an accessible obj? or array?
-// ?firstName = X
-// })
-
-// query = First Last
-// myquerystring::::: var X = query.split(" ")
-// x[0] = match Firstname, && x[1] = match Lastname
-
-//     await sequelize.query(
-//         'SELECT * FROM users WHERE id LIKE :search_name',
-//         {
-//             replacements: { search_name: `${searchTerm}` },
-//             type: QueryTypes.SELECT
-//         }
-//         );
-//         const searchTerm = $('.search').val();
-
-//     await sequelize.query(
-//         'SELECT * FROM users WHERE nickname LIKE :search_name',
-//         {
-//             replacements: { search_name: `${searchTerm}` },
-//             type: QueryTypes.SELECT
-//         }
-//         );
-
-// // name = first/lastname
-// // id = uniqueIdentifier
-// // nickname = nickname
-
-//     // if i search uniqueidentifier - return OBJ -minus psw
-//     app.get('/api/users', checkAuthenticated, async (req, res) => {
-//         const idSearch = await req.user;
-//         const identity = {
-//             uniqueidentifier: idSearch.uniqueidentifier
-//         }
-//     })
-
-//return result array of search results [{firstName: X, nickname: Y},{},{}]
-
-// }

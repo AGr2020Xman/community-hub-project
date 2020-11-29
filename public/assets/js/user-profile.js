8fbc2df22c9a977e652682eaf3fb066e67b29126
Andre Grech <silver.grech@gmail.com>
2020-11-30 04:25:58 +1100

    getting through to backend now!

--- public/assets/js/user-profile.js
+++ public/assets/js/user-profile.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   const updateForm = $('form.update');
-  const firstNameField = $('form#firstNameField');
-  const lastNameField = $('form#lastNameField');
-  const passwordField = $('form#passwordField');
+  const firstNameField = $('#firstNameField');
+  const lastNameField = $('#lastNameField');
+  const passwordField = $('#passwordField');
 
   const handleLoginErr = (err) => {
     $('#alert .msg').text(err.responseJSON);
@@ -11,10 +11,18 @@ $(document).ready(function () {
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   const updateUser = (firstName, lastName, password) => {
-    $.put('/api/users', {
-      firstName: firstName,
-      lastName: lastName,
-      password: password,
+    $.ajax({
+      url: '/api/users',
+      method: 'PUT',
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        password: password,
+      },
+      success: function (req, res) {
+        res.json(this.data);
+        res.redirect('/user-profile');
+      },
     })
       .then((data) => {
         window.location.replace('/user-profile');
@@ -29,26 +37,22 @@ $(document).ready(function () {
     $('.active-user').text(data.nickname);
   });
 
-  $.put('/api/users').then((req, res) => {
-    // When the signup button is clicked, we validate the email and password are not blank
-    updateForm.on('submit', function (event) {
-      event.preventDefault();
-      if (!userData.firstName || !userData.lastName || !userData.password) {
-        $('#loginModalText').text("Please fill in all fields, even if it's with the same details.");
-        $('#loginModal').modal('show').fadeOut(5000);
-        return;
-      }
-      const userData = {
-        firstName: firstNameField.val().trim(),
-        lastName: lastNameField.val().trim(),
-        password: passwordField.val().trim(),
-      };
-
-      // If we have an email and password, run the updateUser function
-      updateUser(userData.firstName, userData.lastName, userData.password);
-      firstNameField.val('');
-      lastNameField.val('');
-      passwordField.val('');
-    });
+  updateForm.on('submit', function (event) {
+    event.preventDefault();
+    const userData = {
+      firstName: firstNameField.val().trim(),
+      lastName: lastNameField.val().trim(),
+      password: passwordField.val().trim(),
+    };
+    if (!userData.firstName || !userData.lastName || !userData.password) {
+      $('#loginModalText').text("Please fill in all fields, even if it's with the same details.");
+      $('#loginModal').modal('show').fadeOut(5000);
+      return;
+    }
+    // If we have an email and password, run the updateUser function
+    updateUser(userData.firstName, userData.lastName, userData.password);
+    firstNameField.val('');
+    lastNameField.val('');
+    passwordField.val('');
   });
 });

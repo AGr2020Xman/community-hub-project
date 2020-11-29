164daa9f9c862f7e8cf17fb6b95de03a6be7bae0
Harish Narain <harishnarain@gmail.com>
2020-11-30 04:03:01 +1100

    Fixed the direct messaging fallback and updated dm cards to have proper heights with overflow.

--- controllers/messaging/messagingServer.js
+++ controllers/messaging/messagingServer.js
@@ -9,23 +9,23 @@ const createServer = (server) => {
 
   const activeUsers = new Set();
 
-  const createNamespace = (sock, namespace) => {
-    sock.on('direct', (room) => {
-      sock.join(room);
+  const createNamespace = (socket, namespace) => {
+    socket.on('direct', (room) => {
+      socket.join(room);
     });
 
-    sock.on('new user', (data) => {
+    socket.on('new user', (data) => {
       socket.userId = data;
       activeUsers.add(data);
       io.of(namespace).emit('new user', [...activeUsers]);
     });
 
-    sock.on('disconnect', () => {
-      activeUsers.delete(sock.userId);
+    socket.on('disconnect', () => {
+      activeUsers.delete(socket.userId);
       io.of(namespace).emit('user disconnected', socket.userId);
     });
 
-    sock.on('wall post', async (data) => {
+    socket.on('wall post', async (data) => {
       io.of(namespace).emit('wall post', data);
       const postData = {
         namespace,
@@ -36,7 +36,7 @@ const createServer = (server) => {
       await db.collection('posts').add(postData);
     });
 
-    sock.on('direct message', async (data) => {
+    socket.on('direct message', async (data) => {
       io.of(namespace).to(data.id).emit('direct message', data);
       const messsageObj = {
         conversationId: data.id,
@@ -47,15 +47,15 @@ const createServer = (server) => {
       await db.collection('conversations').doc(data.id).collection('messages').add(messsageObj);
     });
 
-    sock.on('typing', (data) => {
+    socket.on('typing', (data) => {
       socket.broadcast.emit('typing', data);
     });
   };
 
   // Create direct message namespace
   const directNamespace = io.of('/direct');
-  directNamespace.on('connection', (sock) => {
-    createNamespace(sock, 'direct');
+  directNamespace.on('connection', (socket) => {
+    createNamespace(socket, 'direct');
   });
 
   // import communities
@@ -64,8 +64,8 @@ const createServer = (server) => {
   // Create a namespace for each community
   communities.forEach((communityNamespace) => {
     const namespace = io.of(`/${communityNamespace}`);
-    namespace.on('connection', (sock) => {
-      createNamespace(sock, communityNamespace);
+    namespace.on('connection', (socket) => {
+      createNamespace(socket, communityNamespace);
     });
   });
 };

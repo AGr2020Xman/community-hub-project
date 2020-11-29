a7dd624d97be63d222de90cb501ff7c1ed6b28cb
Andre Grech <silver.grech@gmail.com>
2020-11-30 03:52:13 +1100

    community api expanding

--- controllers/communities/community-api.js
+++ controllers/communities/community-api.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const router = express.Router();
+
 const db = require('../../models');
 
-router.post("/api/community", async (req, res) => {
-    try {
-      const community = {name: req.body.name,}
-        db.Community.create(geo).then(() => {
-        res.status(307);
-        res.json(community)
-        // res.redirect("/community");
-      });
-    } catch (err) {
-      if (err) console.log(`There was an error adding the community:\n ${err}`);
-      res.status(401).json(err);
-    //   res.redirect("/");
-    }
-  });
+const router = express.Router();
 
-  module.exports = router;
+router.post('/api/community', async (req, res) => {
+  try {
+    const community = { name: req.body.name };
+    db.Community.create(geo).then(() => {
+      res.status(307);
+      res.json(community)
+      // res.redirect("/community");
+    });
+  } catch (err) {
+    if (err) console.log(`There was an error adding the community:\n ${err}`);
+    res.status(401).json(err);
+    //   res.redirect("/");
+  }
+});
+
+module.exports = router;

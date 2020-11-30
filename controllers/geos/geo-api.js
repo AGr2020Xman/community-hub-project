cb56d17c9632c7f740d85146e03a077f4c58626f
Andre Grech <silver.grech@gmail.com>
2020-11-30 16:57:34 +1100

    geo api set to mirror comms

--- controllers/geos/geo-api.js
+++ controllers/geos/geo-api.js
@@ -1,15 +1,35 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const db = require('../../models');
 
-router.get('/geos', (req, res) => {
-  res.render('geos');
-})
+router.get('/geos', async (req, res) => {
+  let queryResult;
+  let results;
+  queryResult = await db.Geo.findAll({
+    attributes: ['name'],
+    // include: {
+    //   model: db.Community, attributes: ['id','name']
+    // },
+    raw: true,
+  });
+  results = {
+    searchResult: queryResult,
+  };
+  const hbsArr = [];
+  queryResult.forEach((el, index) => {
+    const commObj = {};
+    commObj.name = el.name;
+    hbsArr.push({ ...commObj });
+  });
+  console.log(hbsArr);
+
+  res.render('geos', { geos: hbsArr });
+});
 
 router.get('/api/geos', async (req, res) => {
-    let queryResult;
-    let results;
-    queryResult = await db.Geo.findAll({
+  let queryResult;
+  let results;
+  queryResult = await db.Geo.findAll({
     attributes: ['name'],
     // include: {
     //   model: db.Community, attributes: ['id','name']
@@ -20,6 +40,6 @@ router.get('/api/geos', async (req, res) => {
     searchResult: queryResult,
   };
   res.json(results);
-})
+});
 
 module.exports = router;

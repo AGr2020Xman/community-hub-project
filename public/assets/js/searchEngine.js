2d27e12b770e6d090bb68f8c7db410cc5c0884ee
Andre Grech <silver.grech@gmail.com>
2020-12-08 19:34:19 +1100

    eslint fixes/prettier

--- public/assets/js/searchEngine.js
+++ public/assets/js/searchEngine.js
@@ -1,47 +1,52 @@
-const db = require("../../../models");
+/* eslint-disable no-underscore-dangle */
+const db = require('../../../models');
 
 $(() => {
-    $(".searchEngine").autocomplete({
-      source: async function (req, res) {
-        pageOptions = await searchAllSites(req.partial);
-        let pageSuggestion = $.map(pageOptions, (element) => {
-          return {
-            label: element.name + " - " + element.CommunityId,
-            value: element.name,
-          };
-        });
-        response(pageSuggestion);
-      },
-      minLength: 3,
-      select: function (event, ui) {},
-      _renderItem: function (ul, item) {
-        return $("<li>")
-          .attr({ "data-value": item.value, class: "list-group-item" })
-          .append(item.label)
-          .appendTo(ul);
-      },
-    });
-})
+  $('.searchEngine').autocomplete({
+    async source (req, res) {
+      pageOptions = await searchAllSites(req.partial);
+      let pageSuggestion = $.map(pageOptions, (element) => {
+        return {
+          label: `${element.name} ${element.CommunityId}`,
+          value: `${element.name}`,
+        };
+      });
+      response(pageSuggestion);
+    },
+    minLength: 3,
+    select (event, ui) {},
+    _renderItem (ul, item) {
+      return $('<li>')
+        .attr({ 'data-value': item.value, class: 'list-group-item' })
+        .append(item.label)
+        .appendTo(ul);
+    },
+  });
+});
 
 // Otherwise we log any errors
 const searchAllSites = () => {
-    console.log();
-    db.myPage.findAll({include: []}) //
-    ("/api/sites", {
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      nickname: nickname,
-      password: password
+  console.log();
+  db.myPage
+    .findAll({ include: [] })(
+      //
+      '/api/sites',
+      {
+        firstName,
+        lastName,
+        email,
+        nickname,
+      password
+      }
+    )
+    .then(function (data) {
+      window.location.replace('/login');
+      // If there's an error, handle it by throwing up a bootstrap alert
     })
-      .then(function(data) {
-        window.location.replace("/login");
-        // If there's an error, handle it by throwing up a bootstrap alert
-      })
-      .catch(handleLoginErr);
-  }
+    .catch(handleLoginErr);
+};
 
-  const handleLoginErr = (err) => {
-    $("#alert .msg").text(err.responseJSON);
-    $("#alert").fadeIn(500);
-  }
+const handleLoginErr = (err) => {
+  $('#alert .msg').text(err.responseJSON);
+  $('#alert').fadeIn(500);
+};
